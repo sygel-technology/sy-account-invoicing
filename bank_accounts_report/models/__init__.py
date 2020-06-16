84709d7e87430a9da89b935ada645086c482d3cd
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-06-16 16:48:39 +0200

    [ADD]Initial

--- bank_accounts_report/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2018 Xavier Piernas <xavier.piernas@qubiq.es>
-# Copyright 2020 Valentin Vinagre <valentin.vinagre@sygel.es>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import account_move
-from . import account_payment_mode
-from . import sale_order
