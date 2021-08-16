3a8b512aaedeea13b9acd8b9ee58a57c1ac91589
manu <manuel.regidor@sygel.es>
2021-08-16 10:09:44 +0200

    mig-14.0-invoice_inside_account_menu

--- invoice_inside_account_menu/__manifest__.py
+++ invoice_inside_account_menu/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Invoicing Inside Account Menu",
     "summary": "Put the Invoicing Menu inside the Accounting Menu",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting, Invoicing",
     "website": "https://www.qubiq.es",
     "author": "QubiQ, Odoo Community Association (OCA)",
