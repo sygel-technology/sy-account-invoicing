50a3808274eb70c18b8c195375d0db4a0add67b7
Manuel Florido <manuel.florido@sygel.es>
2024-07-10 13:37:58 +0200

    [T-5944][IMP] bank_accounts_report,followers_mass_invoice_mailing,invoice_inside_account_menu,invoice_line_display_number,invoice_line_label_without_internal_ref:pre-commit auto fixes

--- invoice_line_display_number/__manifest__.py
+++ invoice_line_display_number/__manifest__.py
@@ -6,20 +6,18 @@
     "summary": "Configure number of invoice lines to be shown",
     "version": "15.0.1.0.0",
     "category": "Invoicing",
-    "website": "https://www.sygel.es",
+    "website": "https://github.com/sygel-technology/sy-account-invoicing",
     "author": "Sygel, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
     "depends": [
-        'account',
+        "account",
     ],
-    "data": [
-        "data/invoice_line_display_number_data.xml"
-    ],
+    "data": ["data/invoice_line_display_number_data.xml"],
     "assets": {
-        'web.assets_backend': [
-            'invoice_line_display_number/static/src/js/form_view.js',
+        "web.assets_backend": [
+            "invoice_line_display_number/static/src/js/form_view.js",
         ]
-    }
+    },
 }
