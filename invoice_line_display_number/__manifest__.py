a42f057719711ea8d1b5d92a14dc281998188184
sygel <alberto.martinez@sygel.es>
2026-05-21 17:48:47 +0200

    [T-9594][IMP] invoice_line_display_number: pre-commit auto fixes

--- invoice_line_display_number/__manifest__.py
+++ invoice_line_display_number/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "15.0.1.0.0",
     "category": "Invoicing",
     "website": "https://github.com/sygel-technology/sy-account-invoicing",
-    "author": "Sygel, Odoo Community Association (OCA)",
+    "author": "Sygel",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
